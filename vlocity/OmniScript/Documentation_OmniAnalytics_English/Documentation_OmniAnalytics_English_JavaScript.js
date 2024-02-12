1545fa672df5030ecdc3999818b89f090a08cfaf
Christophe SEBASTIEN <christophe.sebastien@bearingpoint.com>
2024-02-12 09:24:45 +0100

    Test commit Omniscript

---
+++ vlocity/OmniScript/Documentation_OmniAnalytics_English/Documentation_OmniAnalytics_English_JavaScript.js
@@ -0,0 +1,51 @@
+const productList = [
+                                        {
+                                        'name': 'Apple iPhone 7',
+                                        'id': 'AppIph7',
+                                        'price': '699',
+                                        'brand': 'Apple',
+                                        'category': 'Mobile',
+                                        'variant': 'Black',
+                                        'list': 'Search Results',
+                                        'position': 1
+                                        },
+                                        {
+                                        'name': 'Apple iPhone 8',
+                                        'id': 'AppIph8',
+                                        'price': '749',
+                                        'brand': 'Apple',
+                                        'category': 'Mobile',
+                                        'variant': 'Black',
+                                        'list': 'Search Results',
+                                        'position': 2
+                                        },
+                                        {
+                                        'name': 'Apple iPhone X',
+                                        'id': 'AppIphX',
+                                        'price': '799',
+                                        'brand': 'Apple',
+                                        'category': 'Mobile',
+                                        'variant': 'Black',
+                                        'list': 'Search Results',
+                                        'position': 3
+                                        },
+                                        {
+                                        'name': 'Apple iPhone 11', 
+                                        'id': 'AppIph11',
+                                        'price': '849',
+                                        'brand': 'Apple',
+                                        'category': 'Mobile',
+                                        'variant': 'Black',
+                                        'list': 'Search Results',
+                                        'position': 4
+                                        },
+                                        {
+                                        'name': 'Apple iPhone XR',
+                                        'id': 'AppIphXR',
+                                        'price': '899',
+                                        'brand': 'Apple',
+                                        'category': 'Mobile',
+                                        'variant': 'Black',
+                                        'list': 'Search Results',
+                                        'position': 5
+                                        }];
